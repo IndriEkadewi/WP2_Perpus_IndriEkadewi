11cab77941ac28650fce57e003c16614261b2de2
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-19 13:51:44 +0700

    Update pustaka_helper.php
    mengubah posisi letak

--- application/helpers/pustaka_helper.php
+++ application/helpers/pustaka_helper.php
@@ -3,10 +3,10 @@ fuction cek_login()
 {
 	$sci = get_instance();
 	if (!$sci->session->userdata('email')) {
-		$ci->session->set_flashdata('pesan'), '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login! </div>');
-		redirect('autentifikasi');
+	$ci->session->set_flashdata('pesan'), '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login! </div>');
+	redirect('autentifikasi');
 	} else {
-		$role_id = $sci->session->userdata('role_id');
+	$role_id = $sci->session->userdata('role_id');
 	}
 }
 ?>
