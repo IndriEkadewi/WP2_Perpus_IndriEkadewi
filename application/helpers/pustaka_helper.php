31cb768f84cd3630fb25ecd7f16fdf62ce5bb701
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-20 11:12:49 +0700

    Update pustaka_helper.php
    mengubah isi pada pustaka helper

--- application/helpers/pustaka_helper.php
+++ application/helpers/pustaka_helper.php
@@ -1,12 +1,13 @@
 <?php
-fuction cek_login()
+
+function cek_login()
 {
 	$sci = get_instance();
 	if (!$sci->session->userdata('email')) {
-	$ci->session->set_flashdata('pesan'), '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login! </div>');
-	redirect('autentifikasi');
+		$ci->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Akses ditolak. Anda belum login! </div>');
+		redirect('autentifikasi');
 	} else {
-	$role_id = $sci->session->userdata('role_id');
+		$role_id = $sci->session->userdata('role_id');
 	}
 }
 ?>
