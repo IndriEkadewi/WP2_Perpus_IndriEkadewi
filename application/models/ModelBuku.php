2a0bf68368f623a28e2dfc6fcc517b23ab8f2bb1
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-20 11:17:21 +0700

    Update ModelBuku.php
    mengubah posisi isi dari model buku

--- application/models/ModelBuku.php
+++ application/models/ModelBuku.php
@@ -36,7 +36,7 @@ class ModelBuku extends CI_Model
     }
         $this->db->from('buku');
         return $this->db->get()->row($field);
-}
+    }
 
     //manajemen kategori
     public function getKategori()
@@ -68,8 +68,7 @@ class ModelBuku extends CI_Model
     {
         $this->db->select('buku.id_kategori,kategori.kategori');
         $this->db->from('buku');
-        $this->db->join('kategori','kategori.id = 
-buku.id_kategori');
+        $this->db->join('kategori','kategori.id = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
