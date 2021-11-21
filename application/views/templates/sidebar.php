257713a92e362db5d642378cd3c05a6bac07ffad
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-21 16:50:21 +0700

    Update sidebar.php
    melengkapi isi sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -1,16 +1,30 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebardark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebardark accordion" id="accordionSidebar">
  <!-- Sidebar - Brand -->
  <a class="sidebar-brand d-flex align-items-center 
 justify-content-center" href="index.html">
  <div class="sidebar-brand-icon rotate-n-15">
- <i class="fas fa-book"></i
+ <i class="fas fa-book"></i>
  </div>
  <div class="sidebar-brand-text mx-3">Pustaka 
 Booking</div>
  </a>
  <!-- Divider -->
  <hr class="sidebar-divider">
+
+  <!-- Divider -->
+  <div class="sidebar-heading">
+    Home
+  </div>
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item active">
+    <!-- Nav Item - Dashboard -->
+  <li class="nav-item ">
+    <a class="nav-link pb-0 text-white" href="<?= base_url('buku'); ?>">
+      <i class="fa fa-fw fa-book"></i>
+      <span>Dashboard</span></a>
+  </li>
+  <hr class="sidebar-divider">
  
  <!-- Looping Menu-->
  
