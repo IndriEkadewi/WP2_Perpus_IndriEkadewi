da776a1b32d989d07bafabd01d94729d959d8e84
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-21 16:52:43 +0700

    Update aute_footer.php
    mengedit isi pada aute_footer

--- application/views/templates/aute_footer.php
+++ application/views/templates/aute_footer.php
@@ -5,10 +5,10 @@
 ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core plugin JavaScript-->
-<script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.js"></script>
+<script src="<?= base_url('assets/'); ?>vendor/jqueryeasing/jquery.easing.min.js"></script>
 
 <!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
+<script src="<?= base_url('assets/'); ?>js/sb-admin2.min.js"></script>
 <script>
  $('.alert-message').alert().delay(3000).slideUp('slow');
 </script>
