92ae3483d454e649e8f5bb3c1cfe6f63f29eb218
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-21 16:49:26 +0700

    Update autoload.php
    hapus jarak yang berlebih

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -4,9 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $autoload['packages'] = array();
 
-
 $autoload['libraries'] = array('form_validation', 'session', 'database');
-
 
 $autoload['drivers'] = array();
 
