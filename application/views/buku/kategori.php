7af44e3f7f7d5745cbaaea8b8ab7263291c1b3e5
Indri Ekadewi <ekadewiindry@gmail.com>
2021-11-30 17:01:53 +0700

    Update kategori.php

--- application/views/buku/kategori.php
+++ application/views/buku/kategori.php
@@ -1,7 +1,5 @@
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
-	<?= $this->session->flashdata('pesan'); ?>
 	<div class="row">
 		<div class="col-lg-3">
 			<?php if(validation_errors()){?>
@@ -9,7 +7,6 @@
 					<?= validation_errors();?>
 					</div>
 				<?php } ?>
-				<?= $this->session->flashdata('pesan'); ?>
 				<a href="" class="btn btn-primary mb-3" data-toggle="modal" data-target="#kategoriBaruModal"><i class="fas fa-file-alt"></i> Tambah Kategori</a>
 				<table class="table table-hover">
 					<thead>
@@ -26,10 +23,10 @@
 						foreach ($kategori as $k) { ?> 
 							<tr>
 								<th scope="row"><?= $a++; ?></th>
-								<td><?= $k['kategori']; ?></td>
+								<td><?= $k['nama_kategori']; ?></td>
 								<td>
-									<a href="<?= base_url('buku/ubahkategori/') . $k['id'] ;?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
-									<a href="<?= base_url('buku/hapuskategori/') . $k['id'] ;?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['kategori'];?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i> Hapus</a>
+									<a href="<?= base_url('buku/ubahkategori/').$k['id_kategori'];?>" class="badge badge-info"><i class="fas fa-edit"></i> Ubah</a>
+									<a href="<?= base_url('buku/hapuskategori/').$k['id_kategori'];?>" onclick="return confirm('Kamu yakin akan menghapus <?= $judul.' '.$k['id_kategori'];?> ?');" class="badge badge-danger"><i class="fas fa-trash"></i>Hapus</a>
 								</td>
 							</tr>
 						<?php } ?>
@@ -57,7 +54,7 @@
 			<form action="<?= base_url('buku/kategori'); ?>" method="post">
 				<div class="modal-body">
 					<div class="form-group">
-						<select name="kategori" class="form-control form-control-user"> <option value="">Pilih Kategori</option> <?php $k = ['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','Bahasa','Komik'];
+						<select name="kategori" class="form-control form-control-user"> <option value="">Pilih Kategori</option> <?php $k = ['Sains','Hobby','Komputer','Komunikasi','Hukum','Agama','Populer','Bahasa','Komik', 'Rahasia Keajaiban Bumi'];
 						for ($i=0;$i<9;$i++) { ?> <option value="<?= $k[$i];?>"><?= $k[$i];?></option>
 						<?php } ?>
 					</select>
