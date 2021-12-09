2367ead0da15351086efc54383253fc13465f49f
Indri Ekadewi <ekadewiindry@gmail.com>
2021-12-09 09:16:47 +0700

    Update user.php

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -1,15 +1,14 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class User extends CI_Controller
 {
-    public function __construct()
+    Public function __construct()
     {
         parent::__construct();
         cek_login();
     }
 
-    public function index()
+    Public function index()
     {
         $data['judul'] = 'Profil Saya';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
@@ -20,12 +19,13 @@ class User extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-    public function anggota()
+    Public function anggota()
     {
-        $data['judul'] = 'Data Anggota';
-        $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
-        $this->db->where('role_id', 1);
-        $data['anggota'] = $this->db->get('user')->result_array();
+        $data   = [
+            'judul'     => "Data Anggota",
+            'user'      => $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array(),
+            'anggota'   => $this->db->get_where('user', ['role_id' => 2])->result_array()
+        ];
         $this->load->view('templates/header', $data);
         $this->load->view('templates/sidebar', $data);
         $this->load->view('templates/topbar', $data);
@@ -33,15 +33,13 @@ class User extends CI_Controller
         $this->load->view('templates/footer');
     }
 
-    public function ubah_profil()
+    Public function ubah_profil()
     {
-        $data['judul'] = 'Ubah Profile';
+        $data['judul'] = 'Ubah Profil';
         $data['user'] = $this->ModelUser->cekData(['email' => $this->session->userdata('email')])->row_array();
- 
         $this->form_validation->set_rules('nama', 'Nama Lengkap', 'required|trim', [
             'required' => 'Nama tidak Boleh Kosong'
-            ]
-        );
+        ]);
 
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header', $data);
@@ -55,17 +53,14 @@ class User extends CI_Controller
 
             //jika ada gambar yang akan diupload
             $upload_image = $_FILES['image']['name'];
-
             if ($upload_image) {
                 $config['upload_path'] = './assets/img/profile/';
                 $config['allowed_types'] = 'gif|jpg|png|jpeg';
-                $config['max_size'] = '3000';
+                $config['max_size']     = '3000';
                 $config['max_width'] = '1024';
                 $config['max_height'] = '1000';
                 $config['file_name'] = 'pro' . time();
-
                 $this->load->library('upload', $config);
-
                 if ($this->upload->do_upload('image')) {
                     $gambar_lama = $data['user']['image'];
                     if ($gambar_lama != 'default.jpg') {
@@ -76,13 +71,12 @@ class User extends CI_Controller
                 } else {
                 }
             }
-
             $this->db->set('nama', $nama);
             $this->db->where('email', $email);
             $this->db->update('user');
-            
+
             $this->session->set_flashdata('pesan', '<div class="alert alert-success alert-message" role="alert">Profil Berhasil diubah </div>');
-                redirect('user');
-            }
+            redirect('user');
         }
     }
+}
